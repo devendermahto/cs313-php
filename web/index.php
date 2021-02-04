5b72e878da8ec9ef677115bd7441fcf9bc44ea04
Devender Mahto <devendermahto@gmail.com>
2021-02-04 11:51:06 +0530

    subtake
    subtake

--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
-<?php
-
-  phpinfo();
-
-?>
+<html>
+ <head>
+  <title>Welcome :)</title>
+ </head>
+ <body>
+ <?php echo '<p>Subdomain Tak3over POC </p><br/>';
+   echo '<p>twitter: devendermahto<br/>';
+ echo "Today is a Great Day to secure things: " . date("d/M/Y") . "<br>";  
+   ?> 
+ </body>
+</html>
